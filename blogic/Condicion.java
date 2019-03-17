eb6bf5f672083279bb897f13fd55ab20f7d4d31b
DanielOrtegaZ <dortegaz1500@alumno.ipn.mx>
2019-03-16 20:01:35 -0600

    Creating Business Logic Classes

---
+++ blogic/Condicion.java
@@ -0,0 +1,27 @@
+/*
+ * UNIVERSITY:  INSTITUTO POLITECNICO NACIONAL (IPN)
+ *              ESCUELA SUPERIOR DE COMPUTO (ESCOM)
+ *   SUBJECT:     _
+ *   PROFESSOR:   _
+ *
+ * DESARROLLADORES: Daniel Ortega
+ *
+ * PRACTICE _:  TITULO DE LA PRACTICA
+ *               - DESCRIPCION
+ *		
+*/
+
+public class Class{
+    
+    /* ATRIBUTES */
+    
+    /* CONSTRUCTORS */
+    
+    /* PUBLIC METHODS */
+	
+	/* PRIVATE METHODS */
+    
+    public static void main(String[] args) {
+    
+    }
+}
