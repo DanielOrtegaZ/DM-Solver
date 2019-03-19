a8dada8b7bcaadfa8a4365d78758928a481751c8
DanielOrtegaZ <dortegaz1500@alumno.ipn.mx>
2019-03-19 13:29:20 -0600

    Creating Input Activity for Results

---
+++ app/src/main/java/escom/dm_solver/InputActivity.kt
@@ -0,0 +1,22 @@
+package escom.dm_solver
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_input.*
+
+class InputActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+}
