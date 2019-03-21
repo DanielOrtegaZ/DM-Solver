1961b8f4e0e1e8a7d7ed6692671e71998580d681
DanielOrtegaZ <dortegaz1500@alumno.ipn.mx>
2019-03-20 23:53:19 -0600

    Updating Input Commit

--- app/src/main/java/escom/dm_solver/InputActivity.kt
+++ app/src/main/java/escom/dm_solver/InputActivity.kt
@@ -2,11 +2,19 @@ package escom.dm_solver
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
-
+import android.support.v4.app.DialogFragment
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_input.*
+import kotlinx.android.synthetic.main.input_dialog.*
 
-class InputActivity : AppCompatActivity() {
+class InputActivity : AppCompatActivity(), NoticeDialogFragment.NoticeDialogListener {
+
+    /* ACTIONS */
+    val EDIT_Z : Int = 0
+    val NEW_RULE : Int = 1
+
+    var accion = NEW_RULE
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +25,18 @@ class InputActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+        NoticeDialogFragment("Restriccion","12x+14y <= 12").show(supportFragmentManager,"NoticeDilogFragment")
+    }
+
+    override fun onDialogNegativeClick(dialog: DialogFragment) {
+        var inputFragment = dialog as NoticeDialogFragment
+        Toast.makeText(this,"Hola ${inputFragment.input}",Toast.LENGTH_LONG).show()
+    }
+
+    override fun onDialogPositiveClick(dialog: DialogFragment) {
+        var inputFragment = dialog as NoticeDialogFragment
+        Toast.makeText(this,"Hola ${inputFragment.input}",Toast.LENGTH_LONG).show()
     }
 
 }
