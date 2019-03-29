1e5e1046816610464c7fcd999ac82049e798e639
DanielOrtegaZ <dortegaz1500@alumno.ipn.mx>
2019-03-29 13:31:22 -0600

    Fixing my error

--- app/src/main/java/escom/dm_solver/classes/Restriction.kt
+++ app/src/main/java/escom/dm_solver/classes/Restriction.kt
@@ -12,10 +12,11 @@ class Restriction {
     companion object { // STATIC METHODS / VARIABLES
         fun createRestriction(s:String):Restriction?{
 
-            val regex = """([0-9]*|([0-9]+/[0-9]+))[a-y]((\+|-)([0-9]*|([0-9]+/[0-9]+))[a-y])*(<=|>=)[0-9]+""".toRegex()
-            s = s.trim()
+            //val regex = """([0-9]*|([0-9]+/[0-9]+))[a-y]((\+|-)([0-9]*|([0-9]+/[0-9]+))[a-y])*(<=|>=)[0-9]+""".toRegex()
+            val regex = """([0-9]*|([0-9]+/[0-9]+))[a-y]((\+|-)([0-9]*|([0-9]+/[0-9]+))[a-y])*""".toRegex()
+            var eqText = s.trim()
 
-            if( regex.matches(s) )
+            if( regex.matches(eqText) )
                 return Restriction()
             else
                 return null
