aea0fa3bdf3d7bc639b6be35d36a99c7a84ef448
Martín Eduardo Barriga Vargas <martin.barriga.vargas@gmail.com>
2019-03-29 13:18:03 -0600

    clases añadidas

---
+++ blogic/CondicionFinal.java
@@ -0,0 +1,209 @@
+*
+        * UNIVERSITY:  INSTITUTO POLITECNICO NACIONAL (IPN)
+        *              ESCUELA SUPERIOR DE COMPUTO (ESCOM)
+        *   SUBJECT:     _
+        *   PROFESSOR:   _
+        *
+        * DESARROLLADORES: Martin Barrera
+        *                  Daniel Ortega
+        *
+        * PRACTICE _:  TITULO DE LA PRACTICA
+        *               - DESCRIPCION
+        *
+        */
+
+public class CondicionFinal{
+
+    /* ATRIBUTES */
+    public double coefx;
+    public double coefy;
+    public double coefu;
+    public double coefv;
+    public double coefh1;
+    public double coefh2;
+    public double coefh3;
+    public double coefh4;
+    public double coefh5;
+    public double coefA1;
+    public double coefA2;
+    public double coefA3;
+    public double coefA4;
+    public double coefA5;
+    public String asociado;
+    public int solucion;
+
+    /* CONSTRUCTORS */
+    public CondicionFinal(double coefx, double coefy, double coefu, double coefv, double coefh1, double coefh2, double coefh3, double coefh4, double coefh5, double coefA1, double coefA2, double coefA3, double coefA4, double coefA5, String asociado, int solucion) {
+        this.coefx = coefx;
+        this.coefy = coefy;
+        this.coefu = coefu;
+        this.coefv = coefv;
+        this.coefh1 = coefh1;
+        this.coefh2 = coefh2;
+        this.coefh3 = coefh3;
+        this.coefh4 = coefh4;
+        this.coefh5 = coefh5;
+        this.coefA1 = coefA1;
+        this.coefA2 = coefA2;
+        this.coefA3 = coefA3;
+        this.coefA4 = coefA4;
+        this.coefA5 = coefA5;
+        this.asociado = asociado;
+        this.solucion = solucion;
+    }
+
+    /* PUBLIC METHODS */
+
+    public double getCoefx() {
+        return coefx;
+    }
+
+    public double getCoefy() {
+        return coefy;
+    }
+
+    public double getCoefu() {
+        return coefu;
+    }
+
+    public double getCoefv() {
+        return coefv;
+    }
+
+    public double getCoefh1() {
+        return coefh1;
+    }
+
+    public double getCoefh2() {
+        return coefh2;
+    }
+
+    public double getCoefh3() {
+        return coefh3;
+    }
+
+    public double getCoefh4() {
+        return coefh4;
+    }
+
+    public double getCoefh5() {
+        return coefh5;
+    }
+
+    public double getCoefA1() {
+        return coefA1;
+    }
+
+    public double getCoefA2() {
+        return coefA2;
+    }
+
+    public double getCoefA3() {
+        return coefA3;
+    }
+
+    public double getCoefA4() {
+        return coefA4;
+    }
+
+    public double getCoefA5() {
+        return coefA5;
+    }
+
+    public String getAsociado() {
+        return asociado;
+    }
+
+    public double getSolucion() {
+        return solucion;
+    }
+
+    public void setCoefx(double coefx) {
+        this.coefx = coefx;
+    }
+
+    public void setCoefy(double coefy) {
+        this.coefy = coefy;
+    }
+
+    public void setCoefu(double coefu) {
+        this.coefu = coefu;
+    }
+
+    public void setCoefv(double coefv) {
+        this.coefv = coefv;
+    }
+
+    public void setCoefh1(double coefh1) {
+        this.coefh1 = coefh1;
+    }
+
+    public void setCoefh2(double coefh2) {
+        this.coefh2 = coefh2;
+    }
+
+    public void setCoefh3(double coefh3) {
+        this.coefh3 = coefh3;
+    }
+
+    public void setCoefh4(double coefh4) {
+        this.coefh4 = coefh4;
+    }
+
+    public void setCoefh5(double coefh5) {
+        this.coefh5 = coefh5;
+    }
+
+    public void setCoefA1(double coefA1) {
+        this.coefA1 = coefA1;
+    }
+
+    public void setCoefA2(double coefA2) {
+        this.coefA2 = coefA2;
+    }
+
+    public void setCoefA3(double coefA3) {
+        this.coefA3 = coefA3;
+    }
+
+    public void setCoefA4(double coefA4) {
+        this.coefA4 = coefA4;
+    }
+
+    public void setCoefA5(double coefA5) {
+        this.coefA5 = coefA5;
+    }
+
+    public void setAsociado(String asociado) {
+        this.asociado = asociado;
+    }
+
+    public void setSolucion(double solucion) {
+        this.solucion = solucion;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "CondicionFinal{" +
+                "coefx=" + coefx +
+                ", coefy=" + coefy +
+                ", coefu=" + coefu +
+                ", coefv=" + coefv +
+                ", coefh1=" + coefh1 +
+                ", coefh2=" + coefh2 +
+                ", coefh3=" + coefh3 +
+                ", coefh4=" + coefh4 +
+                ", coefh5=" + coefh5 +
+                ", coefA1=" + coefA1 +
+                ", coefA2=" + coefA2 +
+                ", coefA3=" + coefA3 +
+                ", coefA4=" + coefA4 +
+                ", coefA5=" + coefA5 +
+                ", asociado=" + asociado +
+                ", solucion=" + solucion +
+                '}';
+    }
+    /* PRIVATE METHODS */
+
+
+}
---
+++ blogic/FuncionZj.java
@@ -0,0 +1,363 @@
+*
+        * UNIVERSITY:  INSTITUTO POLITECNICO NACIONAL (IPN)
+        *              ESCUELA SUPERIOR DE COMPUTO (ESCOM)
+        *   SUBJECT:     _
+        *   PROFESSOR:   _
+        *
+        * DESARROLLADORES: Martin Barrera
+        *                  Daniel Ortega
+        *
+        * PRACTICE _:  TITULO DE LA PRACTICA
+        *               - DESCRIPCION
+        *
+        */
+
+public class CondicionFinal{
+
+    /* ATRIBUTES */
+    public double coefx;
+    public double coefy;
+    public double coefu;
+    public double coefv;
+    public double coefh1;
+    public double coefh2;
+    public double coefh3;
+    public double coefh4;
+    public double coefh5;
+    public double coefA1;
+    public double coefA2;
+    public double coefA3;
+    public double coefA4;
+    public double coefA5;
+    public double solucion;
+    public double coefxM;
+    public double coefyM;
+    public double coefuM;
+    public double coefvM;
+    public double coefh1M;
+    public double coefh2M;
+    public double coefh3M;
+    public double coefh4M;
+    public double coefh5M;
+    public double coefA1M;
+    public double coefA2M;
+    public double coefA3M;
+    public double coefA4M;
+    public double coefA5M;
+    public double solucionM;
+
+    /* CONSTRUCTORS */
+    public CondicionFinal(double coefx, double coefy, double coefu, double coefv, double coefh1, double coefh2, double coefh3, double coefh4, double coefh5, double coefA1, double coefA2, double coefA3, double coefA4, double coefA5, double solucion, double coefxM, double coefyM, double coefuM, double coefvM, double coefh1M, double coefh2M, double coefh3M, double coefh4M, double coefh5M, double coefA1M, double coefA2M, double coefA3M, double coefA4M, double coefA5M, double solucionM) {
+        this.coefx = coefx;
+        this.coefy = coefy;
+        this.coefu = coefu;
+        this.coefv = coefv;
+        this.coefh1 = coefh1;
+        this.coefh2 = coefh2;
+        this.coefh3 = coefh3;
+        this.coefh4 = coefh4;
+        this.coefh5 = coefh5;
+        this.coefA1 = coefA1;
+        this.coefA2 = coefA2;
+        this.coefA3 = coefA3;
+        this.coefA4 = coefA4;
+        this.coefA5 = coefA5;
+        this.solucion = solucion;
+        this.coefxM = coefxM;
+        this.coefyM = coefyM;
+        this.coefuM = coefuM;
+        this.coefvM = coefvM;
+        this.coefh1M = coefh1M;
+        this.coefh2M = coefh2M;
+        this.coefh3M = coefh3M;
+        this.coefh4M = coefh4M;
+        this.coefh5M = coefh5M;
+        this.coefA1M = coefA1M;
+        this.coefA2M = coefA2M;
+        this.coefA3M = coefA3M;
+        this.coefA4M = coefA4M;
+        this.coefA5M = coefA5M;
+        this.solucionM = solucionM;
+    }
+    /* PUBLIC METHODS */
+
+    public double getCoefx() {
+        return coefx;
+    }
+
+    public double getCoefy() {
+        return coefy;
+    }
+
+    public double getCoefu() {
+        return coefu;
+    }
+
+    public double getCoefv() {
+        return coefv;
+    }
+
+    public double getCoefh1() {
+        return coefh1;
+    }
+
+    public double getCoefh2() {
+        return coefh2;
+    }
+
+    public double getCoefh3() {
+        return coefh3;
+    }
+
+    public double getCoefh4() {
+        return coefh4;
+    }
+
+    public double getCoefh5() {
+        return coefh5;
+    }
+
+    public double getCoefA1() {
+        return coefA1;
+    }
+
+    public double getCoefA2() {
+        return coefA2;
+    }
+
+    public double getCoefA3() {
+        return coefA3;
+    }
+
+    public double getCoefA4() {
+        return coefA4;
+    }
+
+    public double getCoefA5() {
+        return coefA5;
+    }
+
+    public double getSolucion() {
+        return solucion;
+    }
+
+    public double getCoefxM() {
+        return coefxM;
+    }
+
+    public double getCoefyM() {
+        return coefyM;
+    }
+
+    public double getCoefuM() {
+        return coefuM;
+    }
+
+    public double getCoefvM() {
+        return coefvM;
+    }
+
+    public double getCoefh1M() {
+        return coefh1M;
+    }
+
+    public double getCoefh2M() {
+        return coefh2M;
+    }
+
+    public double getCoefh3M() {
+        return coefh3M;
+    }
+
+    public double getCoefh4M() {
+        return coefh4M;
+    }
+
+    public double getCoefh5M() {
+        return coefh5M;
+    }
+
+    public double getCoefA1M() {
+        return coefA1M;
+    }
+
+    public double getCoefA2M() {
+        return coefA2M;
+    }
+
+    public double getCoefA3M() {
+        return coefA3M;
+    }
+
+    public double getCoefA4M() {
+        return coefA4M;
+    }
+
+    public double getCoefA5M() {
+        return coefA5M;
+    }
+
+    public int getSolucionM() {
+        return solucionM;
+    }
+
+    public void setCoefx(double coefx) {
+        this.coefx = coefx;
+    }
+
+    public void setCoefy(double coefy) {
+        this.coefy = coefy;
+    }
+
+    public void setCoefu(double coefu) {
+        this.coefu = coefu;
+    }
+
+    public void setCoefv(double coefv) {
+        this.coefv = coefv;
+    }
+
+    public void setCoefh1(double coefh1) {
+        this.coefh1 = coefh1;
+    }
+
+    public void setCoefh2(double coefh2) {
+        this.coefh2 = coefh2;
+    }
+
+    public void setCoefh3(double coefh3) {
+        this.coefh3 = coefh3;
+    }
+
+    public void setCoefh4(double coefh4) {
+        this.coefh4 = coefh4;
+    }
+
+    public void setCoefh5(double coefh5) {
+        this.coefh5 = coefh5;
+    }
+
+    public void setCoefA1(double coefA1) {
+        this.coefA1 = coefA1;
+    }
+
+    public void setCoefA2(double coefA2) {
+        this.coefA2 = coefA2;
+    }
+
+    public void setCoefA3(double coefA3) {
+        this.coefA3 = coefA3;
+    }
+
+    public void setCoefA4(double coefA4) {
+        this.coefA4 = coefA4;
+    }
+
+    public void setCoefA5(double coefA5) {
+        this.coefA5 = coefA5;
+    }
+
+    public void setSolucion(double solucion) {
+        this.solucion = solucion;
+    }
+
+    public void setCoefxM(double coefxM) {
+        this.coefxM = coefxM;
+    }
+
+    public void setCoefyM(double coefyM) {
+        this.coefyM = coefyM;
+    }
+
+    public void setCoefuM(double coefuM) {
+        this.coefuM = coefuM;
+    }
+
+    public void setCoefvM(double coefvM) {
+        this.coefvM = coefvM;
+    }
+
+    public void setCoefh1M(double coefh1M) {
+        this.coefh1M = coefh1M;
+    }
+
+    public void setCoefh2M(double coefh2M) {
+        this.coefh2M = coefh2M;
+    }
+
+    public void setCoefh3M(double coefh3M) {
+        this.coefh3M = coefh3M;
+    }
+
+    public void setCoefh4M(double coefh4M) {
+        this.coefh4M = coefh4M;
+    }
+
+    public void setCoefh5M(double coefh5M) {
+        this.coefh5M = coefh5M;
+    }
+
+    public void setCoefA1M(double coefA1M) {
+        this.coefA1M = coefA1M;
+    }
+
+    public void setCoefA2M(double coefA2M) {
+        this.coefA2M = coefA2M;
+    }
+
+    public void setCoefA3M(double coefA3M) {
+        this.coefA3M = coefA3M;
+    }
+
+    public void setCoefA4M(double coefA4M) {
+        this.coefA4M = coefA4M;
+    }
+
+    public void setCoefA5M(double coefA5M) {
+        this.coefA5M = coefA5M;
+    }
+
+    public void setSolucionM(double solucionM) {
+        this.solucionM = solucionM;
+    }
+
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "CondicionFinal{" +
+                "coefx=" + coefx +
+                ", coefy=" + coefy +
+                ", coefu=" + coefu +
+                ", coefv=" + coefv +
+                ", coefh1=" + coefh1 +
+                ", coefh2=" + coefh2 +
+                ", coefh3=" + coefh3 +
+                ", coefh4=" + coefh4 +
+                ", coefh5=" + coefh5 +
+                ", coefA1=" + coefA1 +
+                ", coefA2=" + coefA2 +
+                ", coefA3=" + coefA3 +
+                ", coefA4=" + coefA4 +
+                ", coefA5=" + coefA5 +
+                ", solucion=" + solucion +
+                ", coefxM=" + coefxM +
+                ", coefyM=" + coefyM +
+                ", coefuM=" + coefuM +
+                ", coefvM=" + coefvM +
+                ", coefh1M=" + coefh1M +
+                ", coefh2M=" + coefh2M +
+                ", coefh3M=" + coefh3M +
+                ", coefh4M=" + coefh4M +
+                ", coefh5M=" + coefh5M +
+                ", coefA1M=" + coefA1M +
+                ", coefA2M=" + coefA2M +
+                ", coefA3M=" + coefA3M +
+                ", coefA4M=" + coefA4M +
+                ", coefA5M=" + coefA5M +
+                ", solucionM=" + solucionM +
+                '}';
+    }
+    /* PRIVATE METHODS */
+
+
+}
