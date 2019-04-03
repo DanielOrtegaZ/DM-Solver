3157baab37dea1ec635d36f806078733dd26bac7
Martín Eduardo Barriga Vargas <martin.barriga.vargas@gmail.com>
2019-04-03 10:02:17 -0600

    Simplex/Genetico

--- app/src/main/java/escom/dm_solver/classes/Genetico.kt
+++ app/src/main/java/escom/dm_solver/classes/Genetico.kt
@@ -1,2 +1,81 @@
 package escom.dm_solver.classes
+import kotlin.math.*
 
+class Genetico  constructor(restricciones: ArrayList<Restriction>,funcionZ: Restriction, numVariables: Int, exponente:Int,numVectores:Int){
+
+    var restriccion = restricciones
+    var funcion = funcionZ
+    var numVar = numVariables
+    var exp = exponente.toDouble()
+    var numVect =numVectores
+    var min = ArrayList <Double>()
+    var max = ArrayList <Double>()
+    var mj = ArrayList<Int>()
+    var vector = ArrayList<ArrayList<Double>>() //Los primeros numeros corresponden al binario de las variables, los siguientes a sus valores finales, incuyendo z. Luego siguen porcentajes y porcentajes acumulados respecto a z, finalmente numeros random del 0.1 al 0.10
+    var result = 0.0
+    var coef = 0.0
+    var valorZ = 0.0
+    var valorTotalZ = 0.0
+    var porcentaje = 0.0
+    var porcentajeAcum = 0.0
+    fun calcular(){
+        valorTotalZ = 0.0
+        porcentajeAcum = 0.0
+        for(i in 0 until numVar){
+            min.add(1000000000.0)
+            max.add(-100000000.0)
+        }
+        for(i in 0 until restriccion.size){
+             result = (restriccion.get(i).result.num).toDouble() / (restriccion.get(i).result.den).toDouble()
+            for(j in 0 until restriccion.get(i).coeficientes.size){
+                if(restriccion.get(i).coeficientes.get(j).num != 0 ) {
+                    coef = (restriccion.get(i).coeficientes.get(j).num).toDouble() / (restriccion.get(i).coeficientes.get(j).den).toDouble()
+                    if (coef / result > max[j]) {
+                        max[j] = coef / result
+                    }
+                    if (coef / result < min[j]) {
+                        min[j] = coef / result
+                    }
+                }
+            }
+        }
+        for(i in 0 until numVar){
+            mj[i] = (log((max[i]-min[i])*((10.0).pow(exp)),10.0) / log(2.0,10.0) + 1).toInt()
+        }
+        for(i in 0 until numVect){
+            valorZ = 0.0
+            for(j in 0 until numVar){
+                vector[i].add((0..((2.0).pow(mj[j])-1).toInt()).random().toDouble())
+            }
+            for(j in 0 until numVar){
+                vector[i].add( (min[j] + vector[i][j] * ( (max[j]-min[j]) / ((2.0).pow(mj[j]) -1) ) ) )
+            }
+            for(j in 0 until funcion.coeficientes.size){
+                coef = (funcion.coeficientes.get(j).num).toDouble() / (funcion.coeficientes.get(j).den).toDouble()
+                valorZ += coef * vector[i][j+numVar]
+            }
+            vector[i].add(valorZ)
+            valorTotalZ += valorZ
+        }
+        //mucho ojo con los valores acumulables
+
+        for(i in 0 until numVect){
+            porcentaje = vector[i][numVar*2] / valorTotalZ
+            porcentajeAcum += porcentaje
+            vector[i].add(porcentaje)
+            if(i == numVect -1) vector[i].add(1.0)
+            else vector[i].add(porcentajeAcum)
+            vector[i].add(((1..99).random()).toDouble() /100)
+        }
+
+        for(i in 0 until numVect){
+            for(j in 0 until numVect){
+                if(vector[i][numVar*2+3] <= vector[j][numVar*2+2]){
+                    vector[i].add(j.toDouble())
+                    break;
+                }
+            }
+        }
+    }
+
+}
--- app/src/main/java/escom/dm_solver/classes/Simplex1.kt
+++ app/src/main/java/escom/dm_solver/classes/Simplex1.kt
@@ -1,22 +1,45 @@
 package escom.dm_solver.classes
 
-class Simplex1 {
+import escom.dm_solver.classes.Restriction.Companion.MAYOR_IGUAL
+
+class  Simplex1 {
     companion object {
 
-        fun calcular(restricciones: Restriction,funcionZ: FunctionZ, numRestricciones: int, numMayorIgual: int) {
+        fun calcular(restricciones: ArrayList<Restriction>,funcionZ: FunctionZ, numRestricciones: int, numMayorIgual: int, numeroVar:int) {
+
             var Cj = ArrayList<Fraction>()
             var nombreIndice = ArrayList<String>()
             var exponenteIndice = ArrayList<Fraction>()
-            var valorIndice = ArrayList<Fraction>()
+            var valorIndice = ArrayList<ArrayList<Fraction>>()
             var numZj = ArrayList<Fraction>()
             var mZj = ArrayList<Fraction>()
             var numCjZj = ArrayList<Fraction>()
             var mCjZj = ArrayList<Fraction>()
 
-            for (i in 0 until restricciones.size) {
+            for (i in 0 until numRestricciones) {
 
-                for (j in 0 until restricciones.get(i).size) {
-                    restricciones[i][j] = 2;
+                for (j in 0 until numeroVar) {
+                    valorIndice[i].add(restricciones[j].coeficientes.get(j))
+                }
+                for(j in 0 until numRestricciones){
+                    if(i-numeroVar == j) {
+                        if (restricciones[j].operator.equals(MAYOR_IGUAL)) {
+                            valorIndice[i].add(Fraction(1, 1))
+                        } else {
+                            valorIndice[i].add(Fraction(-1, 1))
+                        }
+                    }
+                    else{
+                        valorIndice[i].add(Fraction(0,1))
+                    }
+                }
+                for(j in 0 until numRestricciones){
+                    if (restricciones[j].operator.equals(MAYOR_IGUAL)) {
+                        valorIndice[i].add(Fraction(1, 1))
+                    } else {
+                        valorIndice[i].add(Fraction(0, 1))
+                    }
+
                 }
             }
         }
